0d838ce9ed6c9c5b984d218417943051c48555a8
arareabe <abekim143@gmail.com>
2023-01-08 01:15:13 -0800

    changed answer model

--- app/models/answer.py
+++ app/models/answer.py
@@ -11,12 +11,12 @@ class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     answer = db.Column(db.String(3000), nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    questionId= db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('questions.id')), nullable=False)
+    questionId= db.Column(db.Integer, db.ForeignKey('questions.id'), nullable=False)
     createdAt = db.Column(DateTime(timezone=True), server_default=now())
     updatedAt = db.Column(DateTime(timezone=True), onupdate=now())
 
     user = db.relationship('User', back_populates='answers')
-    question = db.relationship('Question', back_populates='answers')
+    question = db.relationship('Question', back_populates='answers', foreign_keys=[questionId])
 
     def to_dict(self):
         return {
