2535414904cb2316771fcef660ebc74034894356
arareabe <abekim143@gmail.com>
2023-01-08 01:43:26 -0800

    added cascade

--- app/models/user.py
+++ app/models/user.py
@@ -17,8 +17,8 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(160), nullable=False)
 
-    questions = db.relationship('Question', back_populates='user')
-    answers = db.relationship('Answer', back_populates='user')
+    questions = db.relationship('Question', back_populates='user', cascade='all, delete-orphan')
+    answers = db.relationship('Answer', back_populates='user', cascade='all, delete-orphan')
 
     @property
     def password(self):
