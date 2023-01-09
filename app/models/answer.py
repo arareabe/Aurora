fa085f6b3b5b1b6de0c3af7ea44249873f26afb8
arareabe <abekim143@gmail.com>
2023-01-08 16:36:34 -0800

    render

--- app/models/answer.py
+++ app/models/answer.py
@@ -12,7 +12,7 @@ class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     answer = db.Column(db.String(3000), nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
-    questionId= db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod(Question.id)), nullable=False)
+    questionId= db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod(questions.id)), nullable=False)
     createdAt = db.Column(DateTime(timezone=True), server_default=now())
     updatedAt = db.Column(DateTime(timezone=True), onupdate=now())
 
